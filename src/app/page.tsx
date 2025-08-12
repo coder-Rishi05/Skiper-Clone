44ac75dd989d4b6c2383f2417b20d8a6eb774652
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-13 00:51:42 +0530

    added text effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@
 
 import Example from '@/components/Example'
 import ShareButton from '@/components/ui/share-button'
+import FlipLink from '@/components/ui/text-effect-flipper'
 import WrapButton from '@/components/ui/wrap-button'
-import { ArrowBigDownDash, Banana, Globe, Linkedin, Twitch } from 'lucide-react'
+import { ArrowBigDownDash, Banana, Globe, Linkedin, LucideLinkedin, Twitch } from 'lucide-react'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-     
+    <div className='flex items-center justify-center h-screen' >
+     <FlipLink href='https://x.com/guri_who' >LinkedIn </FlipLink>
+     <FlipLink href='https://x.com/guri_who' >Insta </FlipLink>
+           
+           
     </div>
   )
 }
